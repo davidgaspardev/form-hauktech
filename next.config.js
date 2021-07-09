94cb2835711100779ddfcedf34fdbeb55ad53255
David Gaspar <davidgaspar.dev@gmail.com>
2021-07-09 19:20:57 -0300

    Expose secrets client side

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    EMAIL_JS_USER: process.env.EMAIL_JS_USER,
+    EMAIL_JS_SERVICE: process.env.EMAIL_JS_SERVICE,
+    EMAIL_JS_TEMPLATE: process.env.EMAIL_JS_TEMPLATE
+  }
 }
